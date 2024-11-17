999603934b6df9ce976908c11eb6b6d3f9105deb
Rask <76151039+po1itepeop1e@users.noreply.github.com>
2024-11-17 20:03:31 +0800

    Add files via upload

--- my-project-backend/src/main/java/com/example/utils/Const.java
+++ my-project-backend/src/main/java/com/example/utils/Const.java
@@ -5,6 +5,11 @@ public class Const {
     public final static String JWT_BLACK_LIST = "jwt:blacklist:";
     public final static String VERIFY_EMAIL_LIMIT = "verify:email:limit:";
     public final static String VERIFY_EMAIL_DATA = "verify:email:data:";
+
+    public static final int ORDER_LIMIT = -101;
     public static final int ORDER_CORS = -102;
     public static final Object ROLE_DEFAULT = "user";
+
+    public static final String FLOW_LIMIT_COUNTER = "flow:counter:";
+    public static final String FLOW_LIMIT_BLOCK = "flow:block:";
 }
